686086c75c7ef8b2e68fd367c6916851e223bc7d
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2023-06-04 23:22:03 +0900

    wip

--- script/index.js
+++ script/index.js
@@ -1,14 +1,15 @@
 import { studentInfo } from "./studentInfo.js";
 
-document.title =
-  studentInfo.성명 +
-  " " +
-  studentInfo.학번 +
-  " " +
-  studentInfo.평가연도
-    .toString()
-    .trim()
-    .substring(2) +
-  "년도 " +
-  studentInfo.평가회차 +
-  "차";
+document.title = `${studentInfo.성명} ${studentInfo.학번} ${studentInfo.평가연도
+  .toString()
+  .trim()
+  .substring(2)}년 ${studentInfo.평가회차}차`;
+
+const title = document.querySelector("#title");
+title.innerHTML = `커뮤니케이션디자인전공</br>${studentInfo.평가연도}년도 졸업작품 ${studentInfo.평가회차}차 평가</br>${studentInfo.성명} ${studentInfo.학번}`;
+
+const profBtns = document.querySelectorAll(".subject");
+profBtns.forEach((eachProf, idx) => {
+  const propName = `지도교수${idx + 1}`;
+  eachProf.innerHTML = `${studentInfo[propName]}`;
+});
