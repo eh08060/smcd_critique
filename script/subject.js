2bcec8d582dfdd45bd53aab4e70bddbcc41a0cd2
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2023-06-07 02:40:15 +0900

    almost release ready

--- script/subject.js
+++ script/subject.js
@@ -5,7 +5,18 @@ const propName = `지도교수${index}`;
 document.title = `${studentInfo.성명} ${studentInfo.평가회차}차 ${studentInfo[propName]}교수 지도과목`;
 
 const title = document.querySelector("#title");
-title.innerHTML = `커뮤니케이션디자인전공</br>${studentInfo.평가연도}년도 졸업작품 ${studentInfo.평가회차}차 평가</br>${studentInfo.성명} ${studentInfo.학번}</br>${studentInfo[propName]}교수 지도과목`;
+title.innerHTML = `커뮤니케이션디자인전공</br>${studentInfo.평가연도}년도 졸업작품 ${studentInfo.평가회차}차 평가`;
+const subtitle = document.querySelector("#subtitle");
+subtitle.innerHTML = `${studentInfo.성명} ${studentInfo.학번}</br>${studentInfo[propName]}교수 지도과목`;
+
+const profBtns = document.querySelectorAll(".subject");
+profBtns.forEach((eachProf, idx) => {
+  const propName = `지도교수${idx + 1}`;
+  eachProf.innerHTML = `${studentInfo[propName]}교수님`;
+});
+
+const hiddenBtn = document.getElementById(`subject${index}`);
+hiddenBtn.style.display = "none";
 
 const innerLinks = document.querySelectorAll(".innerLink");
 const innerLinksMap = new Map();
