527502cdf3b01a4eac7a0b50bf84c4de5b7edcd5
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2023-06-13 23:26:16 +0900

    fix reference wrapper issue

--- src/subject3/script/reference.js
+++ src/subject3/script/reference.js
@@ -7,33 +7,3 @@ const reference = [
     link: "",
   },
 ];
-
-let footer;
-let ol;
-reference.forEach((eachRef) => {
-  if (eachRef.author || eachRef.title || eachRef.link) {
-    if (!footer) {
-      footer = document.createElement("footer");
-      const h2 = document.createElement("h2");
-      h2.innerHTML = "References";
-      footer.prepend(h2);
-      ol = document.createElement("ol");
-    }
-    const a = document.createElement("a");
-    a.href = eachRef.link;
-    a.innerHTML = `<li>
-      ${eachRef.author ? `${eachRef.author} ` : `작자 미상 `}(${
-      eachRef.year ? eachRef.year : `연도 미상`
-    }),
-          <em>${eachRef.title ? eachRef.title : `제목 미상`}.</em> ${
-      eachRef.sourceTitle ? eachRef.sourceTitle : ``
-    }
-      </li>`;
-    ol.append(a);
-    console.log(ol);
-  }
-  if (footer) {
-    footer.append(ol);
-    document.querySelector("nav").after(footer);
-  }
-});
